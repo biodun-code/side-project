75e6ee36e07fdad7a0b747d76b5ec081cab79051
biodun-code <abdultoheeb97@gmail.com>
2023-11-14 15:40:09 +0100

    progress

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,16 +1,43 @@
-import React from "react"
-import {useLocation, useNavigate} from 'react-router-dom';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import iconsax from "./icon.svg";
+import messageicon from "./iconsax-2.svg";
+import icontalk from "./iconsax-3.svg";
+import iconsearch from "./iconsax-4.svg";
+import iconschedule from "./iconsax-5.svg";
+import iconsettings from "./settings.svg";
+import "./home.css";
 
-function Home (){
-    const location=useLocation()
+function Home() {
+  const location = useLocation();
 
-    return (
-        <div className="homepage">
+  return (
+    <div className="homepage">
+      <div class="sidebar">
+        <div class="frame">
+          <div class="div">
+            <button class="iconsax">
+              <img src={iconsax} />
+            </button>
+            <button class="iconsax">
+              <img src={messageicon} />{" "}
+            </button>
+            <button class="iconsax">
+              <img src={icontalk} />
+            </button>
 
-            <h1>Hello {location.state.id} and welcome to the home</h1>
-
+            <button class="iconsax">
+              <img src={iconsearch} />
+            </button>
+            <button class="iconsax">
+              <img src={iconschedule} />
+            </button>
+          </div>
         </div>
-    )
+        <img class="iconsax" src={iconsettings} />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
